72c8c4469de7b0c9396b98ae442e81e4a07353ed
TECHMOJO\kishore.logu <kishore.logu@techmojo.in>
2022-09-29 15:58:22 +0530

    Added state to login, navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import { useContext } from "react";
 import { useNavigate   } from 'react-router-dom';
-import { useAuthValue } from '../../context/Auth-context';
+import  AuthContext from '../../context/auth-context';
 import './Header.scss'
 
 function Header(){
   const navigate = useNavigate();
+  const ctx = useContext(AuthContext);
 
   function getUserDetails(){
     //const {currentUser} = useAuthValue();
@@ -26,20 +28,32 @@ function Header(){
     navigate("");
   }
 
+  function logout(){
+    console.log("Logout function called");
+    // logoutUser();
+    ctx.logout();
+  }
+
   return(
-    <div className="header-container">
-      <div className="header-container-navicon" onClick={navigateToLandingPage}>
-        <i className="fa-sharp fa-solid fa-bars"></i>
-        <span>kanban</span>
-      </div>
-      <div className="header-container-navbuttons">
+          <div className="header-container">
+          <div className="header-container-navicon" onClick={navigateToLandingPage}>
+            <i className="fa-sharp fa-solid fa-bars"></i>
+            <span>kanban</span>
+          </div>
+          { !ctx.isLoggedIn && 
+          <div className="header-container-navbuttons">
+            <button onClick={navigateToLogin}> Login </button> 
+            <button onClick={navigateToSignUp}> Signup </button>
+          </div>
+          }
+          { ctx.isLoggedIn && 
+          <div className="header-container-navbuttons">
+            <button onClick={logout}> Logout </button>
+          </div>
+          }
+        </div>
+        )
 
-        <button onClick={navigateToLogin}> Login </button>
-        <button onClick={navigateToSignUp}> Signup </button>
-      </div>
-
-    </div>
-  )
 }
 
 
